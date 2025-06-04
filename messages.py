c3116f07aba45059ee93ecf5f85ce34ca6313880
life8853 <life88853@gmail.com>
2025-06-04 13:16:33 +0200

    Added midi note changer

--- messages.py
+++ messages.py
@@ -4,6 +4,8 @@ import mediapipe as mp
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
 
+MAX_PERSON_INDEX = 1
+BASE_MIDI_NOTE = 60
 playing = False
 
 def sendMessagesFromPersonDataAsync(results: mp.tasks.vision.PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
@@ -23,11 +25,13 @@ def sendMessagesFromPersonDataAsync(results: mp.tasks.vision.PoseLandmarkerResul
         else:
             client.send_message("/changeinstrument",["arpy",0])
 
-        if(len(results.pose_landmarks) > 0):
-            person2 = ActuallyFuckingUsefulPose(results, 0)
+        if(len(results.pose_landmarks) > MAX_PERSON_INDEX):
+            person2 = ActuallyFuckingUsefulPose(results, MAX_PERSON_INDEX)
             if(person2.isCinema(Side.LEFT)):
                 print("reverb")
                 client.send_message("/setreverb",[True])
             else:
                 print("noreverb")
                 client.send_message("/setreverb", [False])
+
+            client.send_message("/setnote", [BASE_MIDI_NOTE + person2.howHighIsHand(Side.RIGHT)])
